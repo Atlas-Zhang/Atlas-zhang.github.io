fb0377af99ce2f8d63dc0cf9ad6e00ba55708d04
zeroingi <1347906894@qq.com>
2026-07-18 11:34:14 +0800

    404 error

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ import plugin, { Mode } from 'vite-plugin-markdown'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   publicPath: '/utils/',
+   base: '/utils/',
   // plugins: [
   //   Vue({
   //     include: [/\.vue$/, /\.md$/], // <--
